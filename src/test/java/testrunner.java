0e8a9c611f496a1828d170af60ec0272e527c37c
ishivamkumarsingh <140041564+ishivamkumarsingh@users.noreply.github.com>
2025-03-16 04:33:32 -0700

    Add files via upload

---
+++ src/test/java/testrunner.java
@@ -0,0 +1,23 @@
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+    features = "src/test/resources/features/SignIn.feature",
+    glue = {"stepdefinition", "utility", "pageobjects"}, 
+    plugin = {
+        "pretty",
+        "html:target/cucumber-html-report.html",
+        "json:target/cucumber-report.json",
+        // Extent Reports adapter
+        
+        // Allure adapter
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+    },
+    monochrome = true
+)
+public class testrunner extends AbstractTestNGCucumberTests {
+    // We do not need to add anything else here unless you want 
+    // to customize data providers or hooks for parallel execution.
+}
